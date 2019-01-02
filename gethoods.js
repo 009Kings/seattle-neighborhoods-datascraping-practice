83ece6d1e5e167c1f99930fc1339bca642dcb71f
009kings <009kings@gmail.com>
2019-01-02 15:21:14 -0800

    got us a sweet sweet object and the start of a database

--- gethoods.js
+++ gethoods.js
@@ -1,6 +1,16 @@
 var request = require("request");
 var cheerio = require("cheerio");
 
-request("https://www.visitseattle.org/things-to-do/neighborhoods/", (error, response, body)=>{
-  console.log(body);
+request("http://www.visitseattle.org/things-to-do/neighborhoods/", (error, response, body)=>{
+  var $ = cheerio.load(body);
+  var neighborhoods = $(".info-window-content").map((index, currentHood)=>{
+    var photo = $(currentHood).find($(".info-window-content-image")).attr("style") || "none";
+    return {
+      name: $(currentHood).find("h4").text(),
+      link: $(currentHood).find("a").attr("href"),
+      photo: photo.slice(23, -2),
+      discription: $(currentHood).find("p").text()
+    }
+  }).get();
+  console.log(neighborhoods);
 });
