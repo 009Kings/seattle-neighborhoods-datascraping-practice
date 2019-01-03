ab5bbd7bfa18e6ef6af1dbfdcf435c3c8447db2f
009kings <009kings@gmail.com>
2019-01-02 16:33:28 -0800

    Check out them stubbed routes

--- controllers/explore.js
+++ controllers/explore.js
@@ -5,4 +5,8 @@ router.get("/", (req, res)=>{
   res.send("Let's go esplorin'");
 })
 
+router.get("/:idx", (req, res)=>{
+  res.send(`You are looking at Neighborhood ${req.params.idx}`);
+})
+
 module.exports = router;
