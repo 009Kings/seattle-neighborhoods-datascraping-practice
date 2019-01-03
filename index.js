52bb9bc85c522e5b60738e590b455f3f56259e08
009kings <009kings@gmail.com>
2019-01-02 16:32:06 -0800

    gots us a basic express app

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var layouts = require("express-ejs-layouts");
+// var methodOverride = require("method-override") // for Deletin'
+
+app.set("view engine", "ejs");
+
+app.use(layouts);
+// app.use(methodOverride("_method")); // for DOING WHAT WE WANT even if HTTP is a butt about it
+app.use("/explore", require("./controllers/explore"));
+// app.use("/admin", require("./controllers/admin")); // For addin' deletin' and editin'
+
+// Routes
+app.get("/", (req, res)=>{
+  res.send("Dis is a homepage y'all");
+  // res.render("home");
+})
+
+app.listen("3000", () => {
+  console.log("You're listening to the ♨︎ smooth sounds of port 3000 ♨︎");
+});
